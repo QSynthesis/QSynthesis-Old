b9ab4af2d01b67e868d8cdd0186ee28ff81fb406
Sine Striker <trueful@163.com>
2021-09-18 19:54:38 +0800

    Update main.cpp

--- Launcher/linux/QSynthesis/main.cpp
+++ Launcher/linux/QSynthesis/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <cstring>
+#include <fstream>
 
 #include <unistd.h>
 #include <linux/limits.h>
 
 using namespace std;
+
+const char strVersion[] = "0.1.76";
 
 int main(int argc, char **argv, char **envp) {
     // Get Executable Full Path
@@ -17,6 +20,39 @@ int main(int argc, char **argv, char **envp) {
     string strDirPath = strExePath.substr(0, strExePath.find_last_of('/'));
     string strAppPath = strDirPath + "/app/QSynthesis";
 
+    // Check Argument
+    if (argc == 2 && !strcmp(argv[1], "-d")) {
+        string filename = strDirPath + "/QSynthesis.desktop";
+
+        // Write .desktop File
+        fstream fs;
+        fs.open(filename, ios::out);
+        if (fs.fail()) {
+            cout << "Failed to create desktop shortcut." << endl;
+            return -1;
+        }
+
+        fs << "[Desktop Entry]" << endl;
+        fs << "Categories=Utility;Audio;" << endl;
+        fs << "Encoding=UTF-8" << endl;
+        fs << "Version=" << strVersion << endl;
+        fs << "Type=Application" << endl;
+        fs << "Terminal=false" << endl;
+        fs << "Exec=\"" << strExePath << "\" %F" << endl;
+        fs << "Name=QSynthesis" << endl;
+        fs << "Icon=" << strDirPath << "/res/logo.png" << endl;
+        fs << "Comment=\"Cross-platform vocal synthesis frontend\"" << endl;
+
+        fs.close();
+
+        // Give Execute Permission
+        string cmd = "chmod +x \"" + filename + "\"";
+        system(cmd.c_str());
+
+        cout << "Successfully create desktop shortcut." << endl;
+        return 0;
+    }
+
     char **newArgv = new char *[argc];
     newArgv[0] = new char[strAppPath.size() + 1];
     strcpy(newArgv[0], strAppPath.c_str());
@@ -24,11 +60,13 @@ int main(int argc, char **argv, char **envp) {
         newArgv[i] = argv[i];
     }
 
-    // Set Working Directory
-    setenv("PWD", strDirPath.c_str(), 1);
-
     // Create Child Process
     if (fork() == 0) {
+        // Set Working Directory
+        const char *currentDir = strDirPath.c_str();
+        chdir(currentDir);
+        setenv("PWD", currentDir, 1);
+
         execve(strAppPath.c_str(), newArgv, envp);
     }
 
