e2274d6b625aa7276874daafc07241a23eea96b7
Sine Striker <trueful@163.com>
2021-10-27 00:54:01 +0800

    Update DataManager.cpp

--- QSynthesis/Application/Managers/DataManager.cpp
+++ QSynthesis/Application/Managers/DataManager.cpp
@@ -8,12 +8,12 @@ DataManager::~DataManager() {
 }
 
 bool DataManager::load() {
-    translate(":/translations/qsynthesis_cn.qm");
-#if defined(Q_OS_MAC)
-    translate(AppPath + "/translations/qt_zh_CN.qm");
-#else
-    translate(QCoreApplication::applicationDirPath() + "/translations/qt_zh_CN.qm");
-#endif
+//    translate(":/translations/qsynthesis_cn.qm");
+//#if defined(Q_OS_MAC)
+//    translate(AppPath + "/translations/qt_zh_CN.qm");
+//#else
+//    translate(QCoreApplication::applicationDirPath() + "/translations/qt_zh_CN.qm");
+//#endif
     return true;
 }
 
